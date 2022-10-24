4d4e3a5722a08fabd169fdb5469437b080dc4a07
ZlataEkert <zlataekert@gmail.com>
2022-10-24 18:05:27 +0300

    save location to local storage

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+// Init storage
+const storage = new Storage;
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init weather object
-const weather = new Weather('Kyiv');
+const weather = new Weather(weatherLocation.city);
 // Init UI
 const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Lviv');
+// Change location event
+document.getElementById('change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+
+  // Change location
+  weather.changeLocation(city);
+
+  // Set location in LS
+  storage.setLocationData(city);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
 
 function getWeather(){
   weather.getWeather()
